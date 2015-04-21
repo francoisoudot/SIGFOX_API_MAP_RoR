20fa05a1782604ee8c0f9c055f283e5ec64f1c4f
francoisoudot <francois.oudot@gmail.com>
2015-04-20 17:55:05 -0700

    Bug fix 20Apr2015

--- app/controllers/sigfox_controller.rb
+++ app/controllers/sigfox_controller.rb
@@ -4,9 +4,9 @@ class SigfoxController < ApplicationController
   end
 
   def devicetype
-    
+
     device_id=params['id']
-    if params['data'][0..15]=="0000000000000000" || params['data'].size!=24
+    if (params['data'][0..15]=="0000000000000000") || (params['data'].size!=24)
       gps=false
     else
       gps=true
@@ -41,16 +41,16 @@ class SigfoxController < ApplicationController
     latestloc.each do |ll|
       hashloc.merge!({"p"+i.to_s => { "lat" => parse_coord(ll.data[0..7]),"lng"=>parse_coord(ll.data[8..15])}})
       i=i+1
-    endx
+    end
     render :json=> hashloc
   end
 
   def parse_coord(coord)
     dec_coord=coord[1..7].to_i(16).to_s(10)
     final_coord=dec_coord[0..dec_coord.size-7].to_f+(dec_coord[dec_coord.size-6..dec_coord.size-1].to_f/600000)
-    if coord[0]=="8"
-      final_coord=-final_coord
-    end
+      if coord[0]=="8"
+        final_coord=-final_coord
+      end
     return final_coord.to_s
   end
 
